eecd6e46b1ec6ad61169a5d26b325c318c2652c0
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-19 18:13:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,7 +14,7 @@ class Anagram
 
 
 # You'll need to iterate over the array of words that the
-# .match method takes as an argument. You will compare each word of that array 
+# .match method takes as an argument. You will compare each word of that array
 # to the word that the Anagram class is initialized with.
 # To determine if they are anagrams, try determining if they are
 # composed of the same letters. Remember that you can split a word into an array
