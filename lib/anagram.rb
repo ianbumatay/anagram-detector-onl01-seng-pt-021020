cea684d8a6cf6d4e1c207dad4005a50263f54298
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-19 18:13:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,14 +2,27 @@
 
 class Anagram
 
-  attr_accessor :name, :word
+  @@all= []
+
+  attr_accessor :name, :words
 
   def initializ(name)
     @name = name
+    @words = word
   end
 
 
 
+# You'll need to iterate over the array of words that the
+# .match method takes as an argument. You will compare each word of that array 
+# to the word that the Anagram class is initialized with.
+# To determine if they are anagrams, try determining if they are
+# composed of the same letters. Remember that you can split a word into an array
+# of letters using some_word.split(""). You can compare two arrays using the ==. For example:
+
+
+def self.match(words)
+  
 
 
 end
