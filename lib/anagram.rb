4f8b1d5a2a9ec64e0197e58fbe2a64d289f6c92f
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-19 19:03:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,24 +10,11 @@ class Anagram
 
 
 
-# How will you determine if one word is an anagram for another?
+ def match(words_array)
 
-# You'll need to iterate over the array of words that the
-# .match method takes as an argument. You will compare each word of that array
-# to the word that the Anagram class is initialized with.
-# To determine if they are anagrams, try determining if they are
-# composed of the same letters. Remember that you can split a word into an array
-# of letters using some_word.split(""). You can compare two arrays using the ==. For example:
-
-
-def match(words_array)
-
-
-
-    words_array.select do |word|
+      words_array.detect do |word|
       @word.split(" ").sort == word.split(" ").sort
-  
-  end
+     end
 end
 
 
